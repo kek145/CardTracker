53034ce11c98a2700b61518ae029ff23e3792a9d
kek145 <yurabogomolov2004@gmail.com>
2024-06-22 18:10:30 +0300

    UPDATE | Added IdentityController

---
+++ backend/CardTracker.Api/Controllers/IdentityController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardTracker.Api.Controllers;
+
+[ApiController]
+[Route("api/identity")]
+public class IdentityController : ControllerBase
+{
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> Login()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route("registration")]
+    public async Task<IActionResult> Registration()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Authorize]
+    [Route("id")]
+    public IActionResult GetUserId()
+    {
+        return Ok(new { userId = 1 });
+    }
+}
